e423dc434f79229a1631f928fe3ca9b767366242
Andre Busche <a.busche@brunel.net>
2014-09-26 09:18:58 +0200

    adjusted project configuration
    Extended the .gitignore file to ignore Eclipse-specific stuff. Furthermore, added a sample main.cpp for testing purposes.

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : C11++.cpp
+// Author      : Andre Busche
+// Version     :
+// Copyright   : LGPL-2.1
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hi" << endl; // prints Hi
+	return 0;
+}
